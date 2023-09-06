4c5a89e1b6e1bbc4e99986068f9e48b810860604
FireHR2004 <fatir12iksan@gmail.com>
2023-09-06 20:22:02 +0700

    Maap baru push. lepi baru bebas skirpci

--- pages/register.vue
+++ pages/register.vue
@@ -1,8 +1,45 @@
 <template>
   <section class="bg-[#146C94] w-full pt-10 font-Poppins">
     <h1 class="font-bold uppercase text-5xl tracking-[20px] text-white text-center">register</h1>
-    <section class="w-full h-[50%] rounded-t-[50px] bg-white mt-10 p-20">
-      <!-- Lanjut besok -->
+    <section class="w-full h-[50%] rounded-t-[50px] bg-white mt-10 p-20 register">
+      <form action="#" class="mx-[211px] mt-[50px] p-10 font-semibold text-[24px] items-center justify-center flex-auto">
+        <table>
+          <tr>
+            <td>
+              <label for="Name" class="label">Name</label>
+            </td>
+            <td>
+              <input class="shadow appearance-none border rounded w-full py-2 px-2 font-normal text-[24px]" size="100" id="name" type="text" placeholder="Name">
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label for="Name" class="label">Email</label>
+            </td>
+            <td>
+              <input class="shadow appearance-none border rounded w-full py-2 px-2 font-normal text-[24px]" size="100" id="email" type="text" placeholder="Email">
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label for="Name" class="label">Username</label>
+            </td>
+            <td>
+              <input class="shadow appearance-none border rounded w-full py-2 px-2 font-normal text-[24px]" size="100" id="Username" type="text" placeholder="Username">
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label for="Name" class="label">Password</label>
+            </td>
+            <td>
+              <input class="shadow appearance-none border rounded w-full py-2 px-2 font-normal text-[24px]" size="100" id="password" type="text" placeholder="Password">
+            </td>
+          </tr>
+        </table>
+
+        <button type="submit" class="tombol">Register</button>
+      </form>
     </section>
   </section>
 </template>
